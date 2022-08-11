231eac249380d03d970e45f01ce348e7323cd6b9
DFB IT <itdesk@dfbnigeria.org.ng>
2022-08-12 00:58:29 +0100

    Add Blogs Page

--- blogs/views.py
+++ blogs/views.py
@@ -1,9 +1,23 @@
 from django.shortcuts import render
 
+from blogs.models import Post
+
 
 def home(request):
-    return render(request, 'blogs/index.html')
+    posts = Post.objects.all()[:6]
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blogs/index.html', context)
 
 
 def contact(request):
     return render(request, 'blogs/contact.html')
+
+
+def blogs(request):
+    posts = Post.objects.all()
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blogs/blogs.html', context)
